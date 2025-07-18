57a7192481b2603a8aad1d0aaf942f64658eb8ca
Per10dev <per10dot.dev@gmail.com>
2025-07-18 21:40:45 +0800

    Remove old action files from Entities

--- RecallCore/Entities/IAction.cs
+++
@@ -1,9 +0,0 @@
-namespace RecallCore.Entities
-{
-    public interface IAction
-    {
-        string Name { get; }
-        int Cost { get; }
-        void Execute(Player self, Player target);
-    }
-} 
--- RecallCore/Memory/MemoryTimeline.cs
+++ RecallCore/Memory/MemoryTimeline.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using RecallCore.Memory;
 
 namespace RecallCore.Memory
 {
